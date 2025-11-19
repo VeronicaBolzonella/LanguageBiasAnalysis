5cf3ab4a7b567ea0ff151de9d4a4c3e533d9fb4a
Veronica <veronica.bolzonella@gmail.com>
2025-11-19 10:18:51 +0100

    removed partition

--- scripts/crawler.sh
+++ scripts/crawler.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#SBATCH --partition=csedu
-#SBATCH --account=csedui00041
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
 #SBATCH --output=output.out
@@ -9,4 +7,4 @@ source /vol/tensusers/vbolzonella/txmm/LanguageBiasAnalysis/.venv/bin/activate
 
 cd /vol/tensusers/vbolzonella/txmm/LanguageBiasAnalysis
 
-python3 crawler.py 
+python3 data/crawler.py 
