a8f71fb64871722f0a28645b03334ea41e59a8f2
Veronica <veronica.bolzonella@gmail.com>
2025-11-23 16:16:24 +0100

    chaned nltk dir

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import argparse
 import os
 import nltk
-nltk_data_dir = '/tmp/nltk_data'
+
+nltk_data_dir = '/vol/tensusers/vbolzonella/txmm'
 os.makedirs(nltk_data_dir, exist_ok=True)
 nltk.data.path.append(nltk_data_dir)
 nltk.download('punkt', download_dir=nltk_data_dir, quiet=True)
